36ba84ce9596dd8d477cdd6a382e2acf4128fab5
Marco Barão <marco.neves@connectcom.univesp.br>
2018-12-07 09:32:46 -0200

    Adds another icons in Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,15 +5,18 @@ import { Header, Logo, Actions, Action } from './styles';
 import VunespLogo from '../../assets/imgs/logo-univesp.png';
 
 import Notify from '../../assets/imgs/alerta.svg';
+import Settings from '../../assets/imgs/config.svg';
+import User from '../../assets/imgs/usuario.svg';
+import Logout from '../../assets/imgs/sair.svg';
 
 const HeaderC = () => (
   <Header>
     <Logo src={VunespLogo} alt="UNIVESP" />
     <Actions>
       <Action icon={Notify}>Notificações</Action>
-      {/* <Action icon={}>Configurações</Action> */}
-      {/* <Action icon={}>Icones</Action> */}
-      {/* <Action icon={}>Sair</Action> */}
+      <Action icon={Settings}>Configurações</Action>
+      <Action icon={User}>Icones</Action>
+      <Action icon={Logout}>Sair</Action>
     </Actions>
   </Header>
 );
